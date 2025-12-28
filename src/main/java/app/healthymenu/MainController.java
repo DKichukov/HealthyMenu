1f60bd6178c40fe97f39925ae5dd0048de17ad87
DKichukov <dzhamal.kichukov@gmail.com>
2025-12-28 13:56:07 +0200

    implement initial setup for food table and category selection

--- src/main/java/app/healthymenu/MainController.java
+++ src/main/java/app/healthymenu/MainController.java
@@ -1,15 +1,35 @@
 package app.healthymenu;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class MainController {
+public class MainController implements Initializable {
 
   @FXML
-  private Label welcomeText;
-
+  private TableView tableFood;
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  private TableColumn colFoodName;
+  @FXML
+  private TableColumn colFoodCategory;
+  @FXML
+  private TableColumn colFoodWeighG;
+  @FXML
+  private TableColumn colFoodWeighOz;
+  @FXML
+  private TextField txtFoodName;
+  @FXML
+  private ChoiceBox comboCategory;
+  @FXML
+  private TextField txtWeight;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+  comboCategory.getItems().addAll("Milk and derivatives","Meat and fish", "Fruits and vegetables", "Other");
   }
 }
