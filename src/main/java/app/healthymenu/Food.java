00cf5ca07fd2509cd936618f9d164f22e7d6f75a
DKichukov <dzhamal.kichukov@gmail.com>
2025-12-28 14:23:12 +0200

    add Food class and implement food table functionality

---
+++ src/main/java/app/healthymenu/Food.java
@@ -0,0 +1,46 @@
+package app.healthymenu;
+
+import java.text.DecimalFormat;
+
+public class Food {
+
+  private String name;
+  private String category;
+  private int weight;
+
+  public Food(String name, String foodCategory, int weight) {
+    this.name = name;
+    this.category = foodCategory;
+    this.weight = weight;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  public String getWeightOz() {
+    DecimalFormat df = new DecimalFormat();
+    df.setMaximumFractionDigits(2);
+    return df.format(weight * 0.035274f);
+  }
+}
--- src/main/java/app/healthymenu/MainController.java
+++ src/main/java/app/healthymenu/MainController.java
@@ -2,25 +2,30 @@ package app.healthymenu;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainController implements Initializable {
 
   @FXML
-  private TableView tableFood;
+  private TableView<Food> tableFood;
   @FXML
-  private TableColumn colFoodName;
+  private TableColumn<Food, String> colFoodName;
   @FXML
-  private TableColumn colFoodCategory;
+  private TableColumn<Food, String> colFoodCategory;
   @FXML
-  private TableColumn colFoodWeighG;
+  private TableColumn<Food, Integer> colFoodWeighG;
   @FXML
-  private TableColumn colFoodWeighOz;
+  private TableColumn<Food, Float> colFoodWeighOz;
   @FXML
   private TextField txtFoodName;
   @FXML
@@ -28,8 +33,40 @@ public class MainController implements Initializable {
   @FXML
   private TextField txtWeight;
 
+  private ObservableList<Food> food;
+
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-  comboCategory.getItems().addAll("Milk and derivatives","Meat and fish", "Fruits and vegetables", "Other");
+    comboCategory.getItems().addAll("Milk and derivatives", "Meat and fish", "Fruits and vegetables", "Other");
+    colFoodName.setCellValueFactory(new PropertyValueFactory<>("name"));
+    colFoodCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+    colFoodWeighG.setCellValueFactory(new PropertyValueFactory<>("weight"));
+    colFoodWeighOz.setCellValueFactory(new PropertyValueFactory<>("weightOz"));
+
+    food = FXCollections.observableArrayList(
+        new Food("Potatos", "Fruits and vegetables", 200),
+        new Food("Chicker", "Meat and fish", 300),
+        new Food("Milkshake", "Milk and derivatives", 250),
+        new Food("Salmon", "Meat and fish", 300)
+    );
+    tableFood.setItems(food);
+  }
+
+  public void addFood(ActionEvent actionEvent) {
+
+    if (txtFoodName.getText().isEmpty() ||
+        comboCategory.getSelectionModel().getSelectedIndex() < 0 ||
+        txtWeight.getText().isEmpty()) {
+
+      Alert dialog = new Alert(Alert.AlertType.ERROR);
+      dialog.setTitle("Error");
+      dialog.setHeaderText("Error adding data");
+      dialog.setContentText("No field can be empty");
+      dialog.showAndWait();
+    } else {
+      food.add(new Food(txtFoodName.getText(),
+                        (String) comboCategory.getSelectionModel().getSelectedItem(),
+                        Integer.parseInt(txtWeight.getText())));
+    }
   }
 }
